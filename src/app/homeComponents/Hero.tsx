ae379978e813520a96c854476a5ba5743c521d35
mdabutalebdev <mdabutaleb.dev@gmail.com>
2025-11-18 21:10:09 +0600

    marge to main

--- src/app/homeComponents/Hero.tsx
+++ src/app/homeComponents/Hero.tsx
@@ -5,6 +5,7 @@ const Hero = () => {
     <div className='container mx-auto px-20'> 
         <h1 className='text-5xl text-center'>Welcome there</h1>
         <p className='text-center w-[500px]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores cupiditate, facilis dignissimos harum architecto ratione laudantium reiciendis accusamus quasi, omnis fugit, eligendi asperiores aut quisquam. Reiciendis nemo, quos vel quasi, vero reprehenderit tempora magni asperiores quis quod qui sunt et! Fugiat ducimus iure blanditiis non veniam alias, corrupti, aut, impedit nemo repudiandae quae distinctio esse quo facilis? Ea, modi consequuntur?</p>
+        <p className='text-center w-[500px]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores cupiditate, facilis dignissimos harum architecto ratione laudantium reiciendis accusamus quasi, omnis fugit, eligendi asperiores aut quisquam. Reiciendis nemo, quos vel quasi, vero reprehenderit tempora magni asperiores quis quod qui sunt et! Fugiat ducimus iure blanditiis non veniam alias, corrupti, aut, impedit nemo repudiandae quae distinctio esse quo facilis? Ea, modi consequuntur?</p>
     </div>
   )
 }
